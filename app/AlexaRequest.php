7dec80908796a6778ec96e01a66994a02c9a20b7
Chris Sadler <zeretar@myhomehub.co.uk>
2020-05-08 12:55:38 +0100

    Added auto generate model docs

--- app/AlexaRequest.php
+++ app/AlexaRequest.php
@@ -4,6 +4,22 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\AlexaRequest
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AlexaRequest newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AlexaRequest newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AlexaRequest query()
+ * @mixin \Eloquent
+ * @property int $id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string $data
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AlexaRequest whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AlexaRequest whereData($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AlexaRequest whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AlexaRequest whereUpdatedAt($value)
+ */
 class AlexaRequest extends Model
 {
     //
--- app/Http/Controllers/AlexaController.php
+++ app/Http/Controllers/AlexaController.php
@@ -10,5 +10,6 @@ class AlexaController extends Controller
     public function index(Request $request) {
         $alexaRequest = new AlexaRequest();
         $alexaRequest->data = json_encode($request->all());
+        $alexaRequest->save();
     }
 }
